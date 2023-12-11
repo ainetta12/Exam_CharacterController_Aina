a2ebb5f7a5c50b5dddbe96a293af85d60d724c39
ainetta12 <120013205+ainetta12@users.noreply.github.com>
2023-12-11 12:37:37 +0100

    jump

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
-{
+/*{
     
        private CharacterController _controller;
        private Transform _camera;
        private float _horizontal;
        private float _vertical;
 
-       [SerializeField] private float _turnSmoothTime;
-       [SerializeField] private float _turnSmoothVelocity;
        [SerializeField] private float _playerSpeed = 4;
        [SerializeField] private float _jumpForce = 5;
+       [SerializeField] private float _turnSmoothTime;
+       [SerializeField] private float _turnSmoothVelocity;
        [SerializeField] private Transform _sensorPosition;
        [SerializeField] private float _sensorRadius = 0.2;
-       [SerializeField] private LayerMask _layer;
+       [SerializeField] private LayerMask _groundLayer;
 
         private bool _isGrounded;
         private float _gravity = -9.81f;
@@ -52,7 +52,7 @@ public class CharacterController : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngle.y;
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
             transform.rotation = Quaternion.Euler(0, smoothAngle, 0);    
-            Vector3 
+            Vector3 moveDirection = Quaternion.Euler (0, targetAngle, 0) * Vector3.forward; 
             _controller.Move(moveDirection * _playerSpeed * Time.deltaTime);
         }
 
@@ -60,8 +60,21 @@ public class CharacterController : MonoBehaviour
         //_anim.SetFloat("VelY", direction.magnitude);
     }
 
-    /*void Jump()
+    void Jump()
     {
-        
-    }*/
+        _isGrounded = Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+        _playerGravity.y += _gravity * Time.deltaTime;
+        _controller.Move(_playerGravity * Time.deltaTime);
+
+        if(_isGrounded && _playerGravity.y < 0)
+        {
+            _playerGravity.y = -2;
+        }
+
+        if(_isGrounded && Input.GetButtonDowm("Jump"))
+        {
+
+        }
+    }
 }
+*/
